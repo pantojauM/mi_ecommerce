0febfcb536f012c7193a565b3fcbc662e347facd
pantojauM <118494911+pantojauM@users.noreply.github.com>
2026-05-11 14:45:30 -0500

    Set up Express server for ecommerce application

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("nuevo cambio");
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Mi Ecommerce funcionando 🚀');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
