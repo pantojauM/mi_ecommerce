275c9bbb837cb5c3560b20e41a930bde04c958f7
jose <juanesdiaz357@gmail.com>
2026-05-15 22:54:13 -0500

    fix lint

--- orders-service/index.js
+++ orders-service/index.js
@@ -1,45 +1,14 @@
 const express = require('express');
-const axios = require('axios');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-// Ruta principal
-app.get('/', (req, res) => {
-    res.send('Gateway Ecommerce funcionando 🚀');
+app.get('/orders', (req, res) => {
+    res.json([
+        { id: 1, product: 'Laptop', total: 2500 },
+        { id: 2, product: 'Mouse', total: 100 }
+    ]);
 });
 
-// USERS
-app.get('/users', async (req, res) => {
-    try {
-        const response = await axios.get('http://users-service:3001/users');
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ error: 'Error en users-service' });
-    }
-});
-
-// PRODUCTS
-app.get('/products', async (req, res) => {
-    try {
-        const response = await axios.get('http://products-service:3002/products');
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ error: 'Error en products-service' });
-    }
-});
-
-// ORDERS
-app.get('/orders', async (req, res) => {
-    try {
-        const response = await axios.get('http://orders-service:3003/orders');
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ error: 'Error en orders-service' });
-    }
-});
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Gateway running on port ${PORT}`);
+app.listen(3003, '0.0.0.0', () => {
+    console.log('Orders Service running on port 3003');
 });
